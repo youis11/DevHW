36d4aece2cc94ff1d586ad7fcf658a448048f598
youis11 <lluis.moreu@gmail.com>
2018-09-19 00:57:28 +0200

    3 handly constructors

--- vectr3/vectr3/main.cpp
+++ vectr3/vectr3/main.cpp
@@ -9,5 +9,22 @@ class vec3 {
 
 public:
 	
+	//handly constructors
+	vec3() {
+		x = 0;
+		y = 0;
+		z = 0;
+	}
 
+	vec3(type a, type b, type c) {
+		x = a;
+		y = b;
+		z = c;
+	}
+
+	vec3(type da, type db) {
+		2x = da;
+		2y = db;
+		z = 0;
+	}
 };
