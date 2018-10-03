377eda9540e730284c36de2a7d56a67ebf6b4a1d
Lluís Moreu <36155329+youis11@users.noreply.github.com>
2018-10-03 14:02:15 +0200

    string improved

--- string class/string class/string.h
+++ string class/string class/string.h
@@ -1,40 +1,94 @@
-#ifndef _STRING
+#ifndef _STRING_H
 
-#define _STRING
+#define _STRING_H
+
+#include <string.h>
+#include <assert.h>
 
 class STRING 
 {
 
+private:
+	char* string = nullptr;
+	unsigned int allocated_memory = 0u;
+
 public:
-	char* text;
 
 	STRING() {}
 	
-	STRING(const STRING &otherString) {
-		text = otherString.text;
+	STRING(const char* string) {
+		assert(string != nullptr);
+		if (string != nullptr) {
+			allocated_memory = strlen(string) + 1;
+			this->string = new char(allocated_memory);
+			strcpy_s(this->string, allocated_memory, string);
+		}
 	}
 
-	STRING(const char* text) {
-		this->text = (char*)text;
+	~STRING() {
+		if (string != nullptr) {
+			delete[] string;
+			string = nullptr;
+			allocated_memory = 0;
+		}
 	}
 
-	STRING operator= (const STRING &otherString) {
-		return text = otherString.text;
+	STRING (const STRING &string) {
+		assert(string.string != nullptr);
+		if (string.string != nullptr) {
+			allocated_memory = strlen(string.string) + 1;
+			this->string = new char(allocated_memory);
+			strcpy_s(this->string, allocated_memory, string.string);
+		}
+
 	}
 
-	STRING operator= (const char* otherText) {
-		 return text = (char*)otherText;
+	STRING operator= (const char* string) {
+		assert(string != nullptr);
+		if (string != nullptr) {
+			allocated_memory = strlen(string) + 1;
+			delete[] string;
+			this->string = new char(allocated_memory);
+			strcpy_s(this->string, allocated_memory, string);
+		}
+		return *this;// this->string = (char*)string;
 	}
 
-	bool operator== (const STRING &otherString) const {
+	STRING operator= (const STRING &string) {
+		assert(string.string != nullptr);
+		if (string.string != nullptr) {
+			allocated_memory = strlen(string.string) + 1;
+			delete[] string.string;
+			this->string = new char(allocated_memory);
+			strcpy_s(this->string, allocated_memory, string.string);
+		}
+		return *this;//this->string = string.string;
+	}
+
+	STRING operator+= (const char* string) {
+		assert(string != nullptr);
+		if (string != nullptr) {
+			allocated_memory = strlen(string) + 1;
+			delete[] string;
+			this->string = new char(allocated_memory);
+			strcpy_s(this->string, allocated_memory, string);
+		}
+		return (this->string += string);
+	}
+
+	/*bool operator== (const STRING &otherString) const {
 		return(text == otherString.text);
 	}
 
 	bool operator== (const char* otherText) const {
 		return(text == otherText);
+	}*/
+
+	const char* GetString() const {
+		return string;
 	}
 };
 
 
-#endif // !_STRING
+#endif // !_STRING_H
 
