fd51743fc0f260f68471cd5e3c280724461e8c60
Lluís Moreu <36155329+youis11@users.noreply.github.com>
2018-10-26 13:55:41 +0200

    DJYSTRA try

--- Dev_class7.8_handout/Motor2D/j1Map.cpp
+++ Dev_class7.8_handout/Motor2D/j1Map.cpp
@@ -69,6 +69,31 @@ void j1Map::PropagateDijkstra()
 	// TODO 3: Taking BFS as a reference, implement the Dijkstra algorithm
 	// use the 2 dimensional array "cost_so_far" to track the accumulated costs
 	// on each cell (is already reset to 0 automatically)
+
+	iPoint curr;
+	if (frontier.Pop(curr))
+	{
+		iPoint neighbors[4];
+		neighbors[0].create(curr.x + 1, curr.y + 0);
+		neighbors[1].create(curr.x + 0, curr.y + 1);
+		neighbors[2].create(curr.x - 1, curr.y + 0);
+		neighbors[3].create(curr.x + 0, curr.y - 1);
+
+		for (uint i = 0; i < 4; ++i)
+		{
+			if (MovementCost(neighbors[i].x, neighbors[i].y) > 0) {
+
+				int newCost = cost_so_far[curr.x][curr.y] + MovementCost(neighbors[i].x, neighbors[i].y);
+
+				if (cost_so_far[neighbors[i].x][neighbors[i].y] == 0 || newCost < cost_so_far[neighbors[i].x][neighbors[i].y])
+				{
+					cost_so_far[neighbors[i].x][neighbors[i].y] = newCost;
+					frontier.Push(neighbors[i], newCost);
+					breadcrumbs.add(neighbors[i]);
+				}
+			}
+		}
+	}
 }
 
 int j1Map::MovementCost(int x, int y) const
